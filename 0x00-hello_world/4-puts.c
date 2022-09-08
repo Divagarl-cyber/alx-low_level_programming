fb68d580036ce79992750617c1418d6ffb6828a8
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-08 13:58:10 -0700

    fantastic

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
-* main - using the put function to print string
-* 
-* Description: using the main function to print
-*  Programming is like building a multilingual puzzle
-*  Return 0
-*/
+ * main - using the put function to print string
+ *
+ * Description: using the main function to print
+ *  Programming is like building a multilingual puzzle
+ *  Return: 0
+ */
 int main(void)
 {
-	puts("\"Programming here i come");
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
