46a8f5adebd94743af9e689569dc7d302f96cd48
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-08 13:33:06 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 /**
-* main - main block
-* Return: 0
+* main - using the put function to print string
+* 
+* Description: using the main function to print
+*  Programming is like building a multilingual puzzle
+*  Return 0
 */
 int main(void)
 {
