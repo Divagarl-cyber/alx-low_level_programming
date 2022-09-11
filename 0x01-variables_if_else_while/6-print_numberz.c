886d7a2a5166de50d7a57cd3bd0499a78cc77afb
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-11 12:10:25 -0700

    yes

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,22 +1,19 @@
 #include <stdio.h>
 /**
- * main - print if the number is postive, zero, or negative
+ * main - a program that prints all single digit
+ * numbers of baase 10 starting from 0 using putchar
  *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- *Return: 0
+  *Return: 0
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	int i;
+
+	for (i - '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
 }
