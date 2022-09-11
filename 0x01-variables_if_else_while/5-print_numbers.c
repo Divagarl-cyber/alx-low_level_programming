335990b7bea12953fcd74297e3d3f9af4691fb0a
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-11 09:56:10 -0700

    pls oo

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,17 +8,17 @@
  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
 	putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		{
+		putchar(ch);
+		}
+	putchar('\n');
+	return (0);
 }
 
 
