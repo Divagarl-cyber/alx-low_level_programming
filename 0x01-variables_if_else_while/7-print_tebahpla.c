e2d1c88c715c6d17408d984ed0a85c02b540c1cd
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-11 09:02:47 -0700

    please

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,23 +1,19 @@
 #include <stdio.h>
 /**
-* main - printing numbers from 0-9 with commas and space between them
+* main - print if the number is postive, zero, or negative
+*
 * Description: using the main function
-* this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
+* this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
 int main(void)
 {
-	int c;
-
-	for (c = 48; c <= 57; c++)
-	{
-		putchar(c);
-		if (c != 57)
-		{
-			putchar(',');
-			putchar(' ');
-		}
-	}
-	putchar('\n');
-	return (0);
+char c;
+for (c = 'z'; c >= 'a'; c--) 
+{
+putchar(c);
 }
+putchar('\n');
+return (0);
+}
+
