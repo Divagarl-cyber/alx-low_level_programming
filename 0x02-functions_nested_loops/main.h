e92943551fa51199001c5ccdc2203ccf232a6b27
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-13 17:24:10 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 int _putchar(char c);                                                             
 void print_alphabet(void);                                                        
 void print_alphabet_x10(void);                                                    
-int _islowere(int c);                                                             
+int _islower(int c);                                                             
 int _isalpha(int);                                                                
 int print_sight(int n);                                                           
 int _abs(int);                                                                    
