c5f2e0853d0f6d56fa74e57710a98181ef242282
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-13 20:39:15 -0700

    task 5

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ void print_alphabet(void);
 void print_alphabet_x10(void);                                                    
 int _islower(int c);                                                             
 int _isalpha(int);                                                                
-int print_sight(int n);                                                           
+int print_sign(int n);                                                           
 int _abs(int);                                                                    
 int print_last_digit(int);                                                        
 void jack_bauer(void);                                                            
