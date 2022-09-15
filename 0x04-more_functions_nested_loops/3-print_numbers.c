b10d60664579cfe1467eb3088a3aa90ad5489fdf
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-15 14:43:40 -0700

    yes

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * print numbers - prints the numbers 0 to 9,
+ * print numbers - prints the numbers from 0 to 9,
  * followed by a new line
  */
 void print_numbers(void);
