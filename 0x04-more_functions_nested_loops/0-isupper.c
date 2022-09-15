718f04027b07a95624cb41a4d002bbe90d5d2c48
Divagarl-cyber <aodeigah26@gmail.com>
2022-09-15 13:02:57 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isupper - checks for uppercase.
- * @c: character to check.
+ * _isupper - checks for uppercase character
+ * @c: the character to be checked
  * Return: 1 if c is uppercase, otherwise return 0.
  */
 int _isupper(int c)
